062b3e6fc016454a100ba9ed95443bab2aada7fe
MuhamedMJakupi <m.jakupi31@gmail.com>
2025-07-09 22:35:00 +0200

    Added 1 line for test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Trying commit");
+        System.out.println("Trying committ");
     }
 }
